40ee9460c9e784a1b4c707961da966ba39a280d7
liustar2011-afk <liustar2011@gmail.com>
2026-09-11 13:20:46 +0800

    Phase4: export structured onscreen composition auditor

--- script_engine/semantic_contract/__init__.py
+++ script_engine/semantic_contract/__init__.py
@@ -15,6 +15,7 @@ from .models import (
     FoundationRecord,
     SemanticDiagnostic,
 )
+from .onscreen_composition import collect_onscreen_composition_diagnostics
 from .onscreen_contract import collect_onscreen_contract_diagnostics
 from .protected_payload import collect_protected_payload_diagnostics
 from .provenance import validate_final_script_provenance
@@ -39,6 +40,7 @@ __all__ = [
     "RELATION_ALLOWED_EVIDENCE_ROLES",
     "SemanticDiagnostic",
     "audit_final_script_semantic_contract",
+    "collect_onscreen_composition_diagnostics",
     "collect_onscreen_contract_diagnostics",
     "collect_protected_payload_diagnostics",
     "collect_provenance_compatibility_diagnostics",
